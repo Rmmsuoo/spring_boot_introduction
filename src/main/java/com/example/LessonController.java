2f36633a3445dbf1ebfacc806668a5cc55a2701f
Noboru Shimomura <rmmsuoorbnauoi@gmail.com>
2023-09-08 00:47:50 +0900

    lesson04実装完了

--- src/main/java/com/example/LessonController.java
+++ src/main/java/com/example/LessonController.java
@@ -3,8 +3,13 @@ package com.example;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.form.SampleForm;
 
 @Controller
 @RequestMapping("/lesson")
@@ -28,6 +33,31 @@ public class LessonController {
     @ResponseBody
     public String test() {
         return "Good Evening!";
+    }
+    @GetMapping("/request_test")
+    // 返り値をレスポンスとして扱います
+    @ResponseBody
+    // name ... 名前、 bloodType ... 血液型
+    // パラメータ名と仮引数名が同じな場合、@RequestParam String nameのようにパラメータ名の指定を省略できます
+    public String getTest(@RequestParam(value = "name", required = false, defaultValue = "名無し") String name
+                          , @RequestParam(value="bloodType",required=false,defaultValue="不明") String bloodType) {
+        return "名前: " + name + "<br>血液型: " + bloodType;
+    }
+ // POST通信で/lesson/request_testにリクエストした場合にpostTestを実行します
+    @PostMapping("/request_test")
+    @ResponseBody
+    public String postTest(SampleForm sampleForm) {
+        return "名前: " + sampleForm.getName() + "<br>血液型: " + sampleForm.getBloodType();
+    }
+//    @GetMapping("/form_test")
+//    public String formTest(SampleForm sampleForm, Model model) {
+//        model.addAttribute("sampleForm", sampleForm);
+//        return "lesson/form_test";
+//    }
+    @GetMapping("/form_test")
+    // SampleFormをModel属性にセットするだけであればModelクラスは不要になります
+    public String formTest(@ModelAttribute SampleForm sampleForm) {
+        return "lesson/form_test";
     }
 }
 
